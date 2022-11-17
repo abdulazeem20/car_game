23e51c752c8eb76d6a08a1c95837bb81d59f37b6
Abdul-Azeez Abdul-Azeem <abdulazeemabdulazeez@gmail.com>
2022-11-17 01:51:32 +0100

    updating the commit

--- assets/scripts/modules/GameComponent.js
+++ assets/scripts/modules/GameComponent.js
@@ -1,15 +1,15 @@
 export function Main () {
-    let main = $(`
+  let main = $(`
         <main>
             
         </main>
     `)
-    main.append(gameContainer());
-    $('body').prepend(main)
+  main.append(gameContainer())
+  $('body').prepend(main)
 }
 
 function gameContainer () {
-    let section = $(`
+  let section = $(`
         <section class="game-container">
             <div class="head">
                 
@@ -19,13 +19,13 @@ function gameContainer () {
             </div>
         </section>
     `)
-    road().insertAfter(section.find('.head'))
-    section.find('.tail').append(gameTail());
-    return section;
+  road().insertAfter(section.find('.head'))
+  section.find('.tail').append(gameTail())
+  return section
 }
 
 function gameTail () {
-    let element = $(`
+  let element = $(`
         <div class="start">
             
         </div>
@@ -38,42 +38,38 @@ function gameTail () {
             </button>
         </div>
     `)
-    element.find('.right-control').on('click', function () {
-        let parent = $(this).parents('.game-container');
-        let car = parent.find('.car.user');
-        let width = car[0].clientWidth;
-        let marginLeft = car.css('margin-left').replace('px', '');
-        console.log(marginLeft);
-        if (marginLeft >= 192 ||
-            (Number(marginLeft) + Number(width) > 198)
-        ) {
+  element.find('.right-control').on('click', function () {
+    let parent = $(this).parents('.game-container')
+    let car = parent.find('.car.user')
+    let width = car[0].clientWidth
 
-            return
-        }
-        car.css('margin-left', Number(marginLeft) + Number(width) + 'px');
-    })
+    let marginLeft = car.css('margin-left').replace('px', '')
+    //console.log(marginLeft);
+    let road = $('.road')
+    console.log(road[0].clientWidth)
+    if (marginLeft >= 192 || Number(marginLeft) + Number(width) > 198) {
+      return
+    }
+    car.css('margin-left', Number(marginLeft) + Number(width) + 'px')
+  })
 
-    element.find('.left-control').on('click', function () {
-        let parent = $(this).parents('.game-container');
-        let car = parent.find('.car.user');
-        let width = car[0].clientWidth;
-        let marginLeft = car.css('margin-left').replace('px', '');
+  element.find('.left-control').on('click', function () {
+    let parent = $(this).parents('.game-container')
+    let car = parent.find('.car.user')
+    let width = car[0].clientWidth
+    let marginLeft = car.css('margin-left').replace('px', '')
 
-        console.log(marginLeft);
-        if (marginLeft <= -48 ||
-            (Number(marginLeft) - Number(width)) < -54
-        ) {
-
-            return
-        }
-        car.css('margin-left', Number(marginLeft) - Number(width) + 'px');
-
-    })
-    return element;
+    //console.log(marginLeft);
+    if (marginLeft <= -48 || Number(marginLeft) - Number(width) < -54) {
+      return
+    }
+    car.css('margin-left', Number(marginLeft) - Number(width) + 'px')
+  })
+  return element
 }
 
 function road () {
-    let container = $(`
+  let container = $(`
         <div class="road">
             <div class="tracks">
                 
@@ -81,19 +77,19 @@ function road () {
         </div>
     `)
 
-    for (let i = 1; i <= 12; i++) {
-        let track = $(`<div class="track"></div>`)
-        container.find('.tracks').append(track)
-    }
-    container.append(Car('carred.png', 'user'))
-    return container;
+  for (let i = 1; i <= 12; i++) {
+    let track = $(`<div class="track"></div>`)
+    container.find('.tracks').append(track)
+  }
+  container.append(Car('carred.png', 'user'))
+  return container
 }
 
-function Car (src, type = "", pos = "",) {
-    let car = $(`
+function Car (src, type = '', pos = '') {
+  let car = $(`
         <div class="car  ${pos.concat(' ', type)} ">
             <img src = "./assets/images/${src}" alt = "" >
         </div >
     `)
-    return car;
+  return car
 }
